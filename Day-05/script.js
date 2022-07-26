3a4f2cdf1abb0e7c23a662ad804088e665a636fa
srinidharan <srinidharan2001@gmail.com>
2022-07-26 09:30:14 +0000

    Day-05

--- Day-05/script.js
+++ Day-05/script.js
@@ -1,7 +1,21 @@
-const button = document.querySelector('.button');
-const input = document.querySelector('.input');
-const search = document.querySelector('.search');
-button.addEventListener('click',() => {
-  search.classList.toggle('active');
-  input.focus()
-})
+const loadingtext =document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+
+var load = 0;
+var int = setInterval(blurring,30);
+
+
+
+function blurring(){
+  load++;
+  if(load>99){
+    clearInterval(int);
+  }
+  loadingtext.innerHTML = `${load}%`;
+  loadingtext.style.opacity = scale(load,0,100,1,0);
+  bg.style.filter = `blur(${scale(load,0,100,20,0)}px)`
+  
+}
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
