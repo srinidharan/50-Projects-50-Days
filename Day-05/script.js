8c81b396c2853cfd10b803ec036f33ee8a38f9ce
srinidharan <srinidharan2001@gmail.com>
2022-07-26 09:26:45 +0000

    Day-05

---
+++ Day-05/script.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('.button');
+const input = document.querySelector('.input');
+const search = document.querySelector('.search');
+button.addEventListener('click',() => {
+  search.classList.toggle('active');
+  input.focus()
+})
