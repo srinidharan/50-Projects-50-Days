db18a454bacfbbaa8241bc54a0dec4f8d0fdfcb3
srinidharan <srinidharan2001@gmail.com>
2022-07-27 06:52:53 +0000

    Day-10

---
+++ Day-10/script.js
@@ -0,0 +1,21 @@
+const joke = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+// USING ASYNC/AWAIT
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  const res = await fetch('https://icanhazdadjoke.com', config)
+
+  const data = await res.json()
+
+  joke.innerHTML = data.joke
+}
