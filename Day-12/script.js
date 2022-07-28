06c246f0a810d9da80aac61e26f1fe72f7e78c0a
srinidharan <srinidharan2001@gmail.com>
2022-07-28 09:12:47 +0000

    Day-12

---
+++ Day-12/script.js
@@ -0,0 +1,10 @@
+const toggles =document.querySelectorAll('.faq-toggle');
+
+toggles.forEach(btn=>{
+    btn.addEventListener('click', () => {
+
+    btn.parentNode.classList.toggle('active');
+    
+    })
+
+})
