5fce58c64071f4636725a57e6f769bdd12479fb8
srinidharan <srinidharan2001@gmail.com>
2022-08-12 05:26:40 +0000

    Day-20

---
+++ Day-20/script.js
@@ -0,0 +1,28 @@
+const buttons = document.querySelectorAll('.ripple');
+
+
+buttons.forEach(button =>  {
+    button.addEventListener('click', function(e) {
+        const x =e.clientX;
+        const y =e.clientY;
+
+
+     //whenever  we click set offset  same value of the x aand y co-ordinates   
+        const buttontop = e.target.offsetTop;
+        const buttonleft =e.target.offsetLeft;
+
+        const xinside = x - buttonleft;
+        const yinside = y- buttontop
+        
+
+        const circle =document.createElement('span')
+        circle.classList.add('circle');
+        circle.style.top = yinside + 'px' ;
+        circle.style.left = xinside +'px';
+
+        this.appendChild(circle);
+
+        setTimeout(() => circle.remove(), 500);
+        
+    })
+})
