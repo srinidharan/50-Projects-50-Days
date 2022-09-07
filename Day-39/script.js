506a6c69221fd1c8beb3142155dc61581aee4f68
srinidharan <srinidharan2001@gmail.com>
2022-09-07 12:13:45 +0000

    Day-39

---
+++ Day-39/script.js
@@ -0,0 +1,9 @@
+const password = document.getElementById('password');
+const background = document.getElementById('background');
+
+password.addEventListener('input' , (e) => {
+    const input = e.target.value;
+    const length = input.length;
+    const blurvalue = (20 - length * 2);
+    background.style.filter = `blur(${blurvalue}px)`
+})
